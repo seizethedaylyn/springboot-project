062cb698d20aa8c77451785c3aa343c70f23fe6b
seizethedaylyn <rlacofls294@gmail.com>
2024-01-16 11:42:12 +0900

    create file

--- springboot-developer/src/main/java/me/rlacofls/springbootdeveloper/repository/BlogRepository.java
+++ springboot-developer/src/main/java/me/rlacofls/springbootdeveloper/repository/BlogRepository.java
@@ -1,2 +1,9 @@
-package me.rlacofls.springbootdeveloper.repository;public interface BlogRepository {
+package me.rlacofls.springbootdeveloper.repository;
+
+import me.rlacofls.springbootdeveloper.domain.Article;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BlogRepository extends JpaRepository<Article, Long>{
 }
