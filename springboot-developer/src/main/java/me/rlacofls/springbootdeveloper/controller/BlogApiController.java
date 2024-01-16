538fa84a26c9a70c2bd40374d4ed8ecaa69deaa5
seizethedaylyn <rlacofls294@gmail.com>
2024-01-16 11:42:35 +0900

    feat: add create find function

--- springboot-developer/src/main/java/me/rlacofls/springbootdeveloper/controller/BlogApiController.java
+++ springboot-developer/src/main/java/me/rlacofls/springbootdeveloper/controller/BlogApiController.java
@@ -1,2 +1,46 @@
-package me.rlacofls.springbootdeveloper.controller;public class BlogApiController {
+package me.rlacofls.springbootdeveloper.controller;
+
+import lombok.RequiredArgsConstructor;
+import me.rlacofls.springbootdeveloper.domain.Article;
+import me.rlacofls.springbootdeveloper.dto.AddArticleRequest;
+import me.rlacofls.springbootdeveloper.dto.ArticleResponse;
+import me.rlacofls.springbootdeveloper.service.BlogService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class BlogApiController {
+
+    private final BlogService blogService;
+
+    @PostMapping("/api/articles")
+    public ResponseEntity<Article> addArticle(@RequestBody AddArticleRequest request){
+        Article savedArticle = blogService.save(request);
+
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(savedArticle);
+    }
+
+    @GetMapping("/api/articles")
+    public ResponseEntity<List<ArticleResponse>> findAllArticles(){
+        List<ArticleResponse> articles = blogService.findAll()
+                .stream()
+                .map(ArticleResponse::new)
+                .toList();
+
+        return ResponseEntity.ok()
+                .body(articles);
+    }
+
+    @GetMapping("/api/articles/{id}")
+    public ResponseEntity<ArticleResponse> findByArticeId(@PathVariable Long id) {
+        Article articleResponse = blogService.findById(id);
+
+        return ResponseEntity.ok()
+                .body(new ArticleResponse(articleResponse));
+    }
 }
