29a1c297a8cfe8f075fc5d8d061ec0adf77c8dae
seizethedaylyn <rlacofls294@gmail.com>
2024-01-16 11:42:48 +0900

    entity

--- springboot-developer/src/main/java/me/rlacofls/springbootdeveloper/domain/Article.java
+++ springboot-developer/src/main/java/me/rlacofls/springbootdeveloper/domain/Article.java
@@ -1,2 +1,31 @@
-package me.rlacofls.springbootdeveloper.domain;public class Article {
+package me.rlacofls.springbootdeveloper.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Article {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", updatable = false)
+    private Long id;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "content", nullable = false)
+    private String content;
+
+    @Builder
+    public Article(String title, String content){
+        this.title = title;
+        this.content = content;
+    }
+
 }
