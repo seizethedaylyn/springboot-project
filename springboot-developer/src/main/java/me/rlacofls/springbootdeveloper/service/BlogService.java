297a7afa4960b7e3c91457c1bc56775337af54fa
seizethedaylyn <rlacofls294@gmail.com>
2024-01-16 11:41:41 +0900

    feat: find by Id

--- springboot-developer/src/main/java/me/rlacofls/springbootdeveloper/service/BlogService.java
+++ springboot-developer/src/main/java/me/rlacofls/springbootdeveloper/service/BlogService.java
@@ -1,2 +1,28 @@
-package me.rlacofls.springbootdeveloper.service;public class BlogService {
+package me.rlacofls.springbootdeveloper.service;
+
+import lombok.RequiredArgsConstructor;
+import me.rlacofls.springbootdeveloper.domain.Article;
+import me.rlacofls.springbootdeveloper.dto.AddArticleRequest;
+import me.rlacofls.springbootdeveloper.repository.BlogRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class BlogService {
+    private final BlogRepository blogRepository;
+
+    public Article save(AddArticleRequest request){
+        return blogRepository.save(request.toEntity());
+    }
+
+    public List<Article> findAll(){
+        return blogRepository.findAll();
+    }
+
+    public Article findById(long id){
+        return blogRepository.findById(id)
+                .orElseThrow(()-> new IllegalArgumentException("not found: " + id));
+    }
 }
