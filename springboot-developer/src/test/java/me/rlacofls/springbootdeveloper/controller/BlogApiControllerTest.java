9a84f2b8378618f64b4a1c241de37ed60d4eb869
seizethedaylyn <rlacofls294@gmail.com>
2024-01-16 11:47:36 +0900

    fix: find by id 내용 추가

--- springboot-developer/src/test/java/me/rlacofls/springbootdeveloper/controller/BlogApiControllerTest.java
+++ springboot-developer/src/test/java/me/rlacofls/springbootdeveloper/controller/BlogApiControllerTest.java
@@ -94,11 +94,21 @@ class BlogApiControllerTest {
 
     @DisplayName("findArticle: 블로그 글 조회에 성공")
     @Test
-    public void findArticle() throws Exception{
+    public void findArticle() throws Exception {
         final String url = "/api/articles/{id}";
-        final String title;
-        final String content;
+        final String title = "title";
+        final String content = "content";
 
+        Article savedArticle = blogRepository.save(Article.builder()
+                .title(title)
+                .content(content)
+                .build());
 
+        final ResultActions resultActions = mockMvc.perform(get(url, savedArticle.getId()));
+
+        resultActions
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.content").value(content))
+                .andExpect(jsonPath("$.title").value(title));
     }
 }
